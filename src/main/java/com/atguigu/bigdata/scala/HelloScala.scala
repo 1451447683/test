87b5bdc2a6d0c6c9c29f4875d776b059d7c46b8e
zhiling <zhiling@atguigu.com>
2020-06-23 08:49:10 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/bigdata/scala/HelloScala.scala
@@ -0,0 +1,9 @@
+package com.atguigu.bigdata.scala
+
+object HelloScala {
+  def main (args : Array[String]): Unit = {
+    System.out.println("Hello Scala")
+    println("Hello Scala")
+  }
+
+}
---
+++ src/main/scala/com/atguigu/bigdata/scala/chapter01/Scala01_Hello.scala
@@ -0,0 +1,9 @@
+package com.atguigu.bigdata.scala.chapter01
+
+object Scala01_Hello {
+  def main(args: Array[String]): Unit = {
+    System.out.println("Hello Scala")
+    println("Hello Scala")
+  }
+
+}
